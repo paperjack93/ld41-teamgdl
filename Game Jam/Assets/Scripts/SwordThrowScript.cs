e7a3509fcfdd95ac54436b7db30df412a230b085
paperjack <paperjack93@gmail.com>
2018-04-21 15:37:00 +0300

    Polish

--- Game Jam/Assets/Scripts/SwordThrowScript.cs
+++ Game Jam/Assets/Scripts/SwordThrowScript.cs
@@ -13,34 +13,39 @@ public class SwordThrowScript : MonoBehaviour {
 
 	Vector3 _orgMousePos;
 	Rigidbody2D _rigidBody;
+	Collider2D _collider;
 	ContactFilter2D _filter;
 	float _amountInside = 0f;
+	bool _isInGround = true;
 	bool _canShoot = true;
 	bool _isLaunched = false;
 	bool _isAiming = false;
+	bool _isMovingOutOfGround = false;
 	Collider2D[] _colliders = new Collider2D[5];
+	Collider2D _groundCollider;
 
 	void Start () {
 		_rigidBody = GetComponent<Rigidbody2D>();
+		_collider = GetComponent<Collider2D>();
 		_filter.NoFilter();
 	}
 
 	void Update () {
 		if (Input.GetButtonDown("Fire1") && _canShoot) {
-	 		_orgMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-	 		_isAiming = true;
-	 		aimReticle.SetActive(true);
-	 		aimPointer.SetActive(true);
-	 		aimReticle.transform.rotation = Quaternion.identity;
-
+			if(_isInGround && _groundCollider != null && !_isMovingOutOfGround) {
+				_isMovingOutOfGround = true;
+				transform.DOMoveY(transform.position.y - _collider.Distance(_groundCollider).distance + 0.5f, 0.2f).OnComplete(()=>{
+					_isInGround = false;
+					_isMovingOutOfGround = false;
+					if(Input.GetButton("Fire1") && _canShoot) StartAiming();
+				});
+			} else StartAiming();
 	    } else if (Input.GetButtonUp("Fire1") && _isAiming) {	    	
 	    	_canShoot = false;
 	    	_isLaunched = true;
 	    	_isAiming = false;
 	 		Vector3 delta = _orgMousePos - Camera.main.ScreenToViewportPoint(Input.mousePosition);
-	 		Debug.Log(delta.magnitude);
 	 		delta = Vector3.ClampMagnitude(delta, maxMagnitude);
-	 		transform.position -= transform.up + transform.up * _amountInside;
 	 		_rigidBody.simulated = true;
 	 		_rigidBody.AddForce(delta*maxThrowForce, ForceMode2D.Impulse);
 
@@ -48,12 +53,7 @@ public class SwordThrowScript : MonoBehaviour {
 	 		aimPointer.SetActive(false);
 	    }
 
-	    if(_isAiming){
-	    	Vector3 delta = _orgMousePos - Camera.main.ScreenToViewportPoint(Input.mousePosition);
-	    	delta = Vector3.ClampMagnitude(delta, maxMagnitude);
-         	aimPointer.transform.up = delta;
-         	aimPointer.transform.localScale = new Vector3(1f,1+delta.magnitude*6f,1f);
-	    }
+	    if(_isAiming) UpdateReticles();
 	}
 
 	void FixedUpdate(){
@@ -69,6 +69,22 @@ public class SwordThrowScript : MonoBehaviour {
 		VelocityCheck();
 	}
 
+	void StartAiming(){
+		_orgMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		_isAiming = true;
+		aimReticle.transform.rotation = Quaternion.identity;
+		UpdateReticles();
+		aimReticle.SetActive(true);
+		aimPointer.SetActive(true);
+	}
+
+	void UpdateReticles(){
+		Vector3 delta = _orgMousePos - Camera.main.ScreenToViewportPoint(Input.mousePosition);
+	    delta = Vector3.ClampMagnitude(delta, maxMagnitude);
+         aimPointer.transform.up = delta;
+         aimPointer.transform.localScale = new Vector3(1f,1+delta.magnitude*6f,1f);
+	}
+
 	void VelocityCheck(){
 		if(_rigidBody.velocity.magnitude < maxVelocity || !_rigidBody.simulated) return;
 		_rigidBody.velocity = Vector3.ClampMagnitude(_rigidBody.velocity, maxVelocity);
@@ -79,7 +95,7 @@ public class SwordThrowScript : MonoBehaviour {
 			if(collider == null) continue;
 		    if(collider.tag == "Enemy") OnHitEnemy(collider.GetComponent<EnemyScript>());
 		    else if(collider.tag == "Armor") OnHitArmor(collider.GetComponent<ArmorScript>());
-		    else if(collider.tag == "Ground") OnHitGround();
+		    else if(collider.tag == "Ground") OnHitGround(collider);
 		}
 	} 
 
@@ -96,7 +112,7 @@ public class SwordThrowScript : MonoBehaviour {
 		Debug.Log("Hit Armor");
 	}
 
-	void OnHitGround(){
+	void OnHitGround(Collider2D collider){
 		if(!_isLaunched) return;
 		_amountInside = _rigidBody.velocity.magnitude * 0.1f;
 		transform.position += transform.up * _amountInside;
@@ -104,6 +120,8 @@ public class SwordThrowScript : MonoBehaviour {
 		_rigidBody.velocity = Vector2.zero;
 		_canShoot = true;
 		_isLaunched = false;
+		_isInGround = true;
+		_groundCollider = collider;
 		Debug.Log("Hit ground");
 	}
 
