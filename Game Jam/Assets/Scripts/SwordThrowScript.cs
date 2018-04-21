fa4aeeadea8c469385ec83a0431bf9521372a0d3
paperjack <paperjack93@gmail.com>
2018-04-21 11:46:35 +0300

    Sword now points to where you throw it

--- Game Jam/Assets/Scripts/SwordThrowScript.cs
+++ Game Jam/Assets/Scripts/SwordThrowScript.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SwordThrowScript : MonoBehaviour {
 
 	public float maxThrowForce = 1f;
 	Vector3 _orgMousePos;
 	Rigidbody2D _rigidBody;
+	ContactFilter2D _filter;
+	float _amountInside = 0f;
 
 	void Start () {
 		_rigidBody = GetComponent<Rigidbody2D>();
+		_filter.NoFilter();
 	}
 
 	void Update () {
@@ -17,8 +21,25 @@ public class SwordThrowScript : MonoBehaviour {
 	 		_orgMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 	    } else if (Input.GetButtonUp("Fire1")) {
 	 		Vector3 delta = _orgMousePos - Camera.main.ScreenToViewportPoint(Input.mousePosition);
+	 		transform.position -= transform.up;
+	 		_rigidBody.simulated = true;
 	 		_rigidBody.AddForce(delta*maxThrowForce, ForceMode2D.Impulse);
-	 		//_rigidBody.MoveRotation();
 	    }
+	}
+
+	void FixedUpdate(){
+		Collider2D[] colliders = new Collider2D[5];
+		int colliderCount = _rigidBody.OverlapCollider(_filter, colliders);
+		if(colliderCount < 1){
+	 		float angle = Vector2.SignedAngle(Vector2.up, _rigidBody.velocity);
+			_rigidBody.MoveRotation(angle);
+		} else {
+			if(!_rigidBody.IsSleeping()){
+				//_amountInside = _rigidBody.velocity.magnitude * 0.1f;
+				transform.position += transform.up * _amountInside;
+				_rigidBody.simulated = false;
+				_rigidBody.velocity = Vector2.zero;
+			}
+		}	 	
 	}
 }
