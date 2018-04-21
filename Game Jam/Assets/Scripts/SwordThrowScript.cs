275974de1340ba362f0a3e38437c55cea8c25bdd
paperjack <paperjack93@gmail.com>
2018-04-21 11:54:31 +0300

    Sword now sticks to things

--- Game Jam/Assets/Scripts/SwordThrowScript.cs
+++ Game Jam/Assets/Scripts/SwordThrowScript.cs
@@ -6,10 +6,12 @@ using DG.Tweening;
 public class SwordThrowScript : MonoBehaviour {
 
 	public float maxThrowForce = 1f;
+	public float maxVelocity = 10f;
 	Vector3 _orgMousePos;
 	Rigidbody2D _rigidBody;
 	ContactFilter2D _filter;
 	float _amountInside = 0f;
+	bool _canShoot = true;
 
 	void Start () {
 		_rigidBody = GetComponent<Rigidbody2D>();
@@ -17,11 +19,11 @@ public class SwordThrowScript : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetButtonDown("Fire1") && _canShoot) {
 	 		_orgMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-	    } else if (Input.GetButtonUp("Fire1")) {
+	    } else if (Input.GetButtonUp("Fire1") && _canShoot) {
 	 		Vector3 delta = _orgMousePos - Camera.main.ScreenToViewportPoint(Input.mousePosition);
-	 		transform.position -= transform.up;
+	 		transform.position -= transform.up + transform.up * _amountInside;
 	 		_rigidBody.simulated = true;
 	 		_rigidBody.AddForce(delta*maxThrowForce, ForceMode2D.Impulse);
 	    }
@@ -34,12 +36,20 @@ public class SwordThrowScript : MonoBehaviour {
 	 		float angle = Vector2.SignedAngle(Vector2.up, _rigidBody.velocity);
 			_rigidBody.MoveRotation(angle);
 		} else {
-			if(!_rigidBody.IsSleeping()){
-				//_amountInside = _rigidBody.velocity.magnitude * 0.1f;
+			if(_rigidBody.simulated){
+				_amountInside = _rigidBody.velocity.magnitude * 0.1f;
 				transform.position += transform.up * _amountInside;
 				_rigidBody.simulated = false;
 				_rigidBody.velocity = Vector2.zero;
+				_canShoot = true;
 			}
-		}	 	
+		}
+
+		VelocityCheck();
+	}
+
+	void VelocityCheck(){
+		if(_rigidBody.velocity.magnitude < maxVelocity) return;
+		_rigidBody.velocity = _rigidBody.velocity.normalized * maxVelocity;
 	}
 }
