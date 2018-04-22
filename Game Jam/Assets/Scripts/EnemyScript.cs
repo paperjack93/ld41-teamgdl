323befa5534da986d7c348b329bac4d0d6166376
paperjack <paperjack93@gmail.com>
2018-04-22 18:02:24 +0300

    Added slime autodestruction if too far away from screen

--- Game Jam/Assets/Scripts/EnemyScript.cs
+++ Game Jam/Assets/Scripts/EnemyScript.cs
@@ -12,6 +12,7 @@ public class EnemyScript : MonoBehaviour {
 
 	void Start () {
 		if(faceRight) transform.localScale = Vector3.Scale(transform.localScale,new Vector3(-1,1,1));
+		InvokeRepeating("CheckPos", 1f, 1f);
 	}
 	
 	public void OnHit(){
@@ -19,6 +20,10 @@ public class EnemyScript : MonoBehaviour {
 		if(killFx != null) Instantiate(killFx, transform.position, Quaternion.identity); 
 		Camera.main.DOShakePosition(0.25f,0.25f, 3);
 		Destroy(gameObject);
+	}
+
+	void CheckPos(){
+		if(transform.position.magnitude>60f) Destroy(gameObject);
 	}
 
 }
